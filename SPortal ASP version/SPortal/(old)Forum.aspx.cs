4b805f7713acce560df7c9a6c421c56f1fce2420
XcyTheR101 <aspeling.johan@gmail.com>
2013-09-21 11:13:41 +0200

    Forum added to Master Page

--- SPortal ASP version/SPortal/(old)Forum.aspx.cs
+++ SPortal ASP version/SPortal/(old)Forum.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace SPortal
 {
-    public partial class WebForm4 : System.Web.UI.Page
+    public partial class Forum : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
