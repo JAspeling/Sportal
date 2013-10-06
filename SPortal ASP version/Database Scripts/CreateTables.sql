3beaa35033c4da03f92735f63f14a03737e52301
OCdeBruyn <ocdebruyn@gmail.com>
2013-10-06 12:15:48 +0200

    Query automation
    Runs all create table and create proc scripts
    
    Ensure that the sql cmd is enable on both these scripts when you run
    Query window -> SQL CMD MODE

---
+++ SPortal ASP version/Database Scripts/CreateTables.sql
@@ -0,0 +1,13 @@
+GO
+:r "C:\Database Scripts\Create Tables\1 - Institution.sql"
+:r "C:\Database Scripts\Create Tables\2 - PostType.sql"
+:r "C:\Database Scripts\Create Tables\3 - UserType.sql"
+:r "C:\Database Scripts\Create Tables\3.1 - Users.sql"
+:r "C:\Database Scripts\Create Tables\4 - GroupHouse.sql"
+:r "C:\Database Scripts\Create Tables\5 - MemberRole.sql"
+:r "C:\Database Scripts\Create Tables\6 - GroupMembers.sql"
+:r "C:\Database Scripts\Create Tables\7 - Post.sql"
+:r "C:\Database Scripts\Create Tables\8 - Topic.sql"
+:r "C:\Database Scripts\Create Tables\8.1 - TopicType.sql"
+:r "C:\Database Scripts\Create Tables\9 - Topic Post.sql"
+:r "C:\Database Scripts\Create Tables\10 - PostViews.sql"
