059d74c4a9c5b9e161bc45f6ea223441d0b3f4c0
vee921 <vee921@webmail.co.za>
2013-10-20 17:29:13 +0200

    Created secondary file groups for partitions

--- SPortal ASP version/Database Scripts/Creation Database/DATABASE CREATION.sql
+++ SPortal ASP version/Database Scripts/Creation Database/DATABASE CREATION.sql
@@ -1,15 +1,38 @@
 CREATE DATABASE SPortalDb
 ON PRIMARY
 (
-NAME        = SportalDb,
-FILENAME    = 'C:\DATABASE\SportalDb.mdf',
+NAME        = SPortalDb,
+FILENAME    = 'C:\DATABASE\SPortalDb.mdf',
 SIZE        = 5MB,
 MAXSIZE     = UNLIMITED,
 FILEGROWTH  = 10
-) 
+),
+FILEGROUP SECONDARY 
+(
+	NAME        = SPortalDb2,
+	FILENAME    = 'C:\DATABASE\SPortalDb2.ndf',
+	SIZE        = 5MB,
+	MAXSIZE     = UNLIMITED,
+	FILEGROWTH  = 10
+),
+(
+	NAME        = SPortalDb4,
+	FILENAME    = 'C:\DATABASE\SPortalDb3.ndf',
+	SIZE        = 5MB,
+	MAXSIZE     = UNLIMITED,
+	FILEGROWTH  = 10
+),
+(
+	NAME        = SportDb3,
+	FILENAME    = 'C:\DATABASE\SportDb4.ndf',
+	SIZE        = 5MB,
+	MAXSIZE     = UNLIMITED,
+	FILEGROWTH  = 10
+)
+
 
 LOG ON
 (
-NAME        = SportalDb_Log,
-FILENAME    = 'C:\DATABASE\SportalDb_Log.ldf'
+NAME        = SPortalDb_Log,
+FILENAME    = 'C:\DATABASE\SPortal_Log.ldf'
 )
